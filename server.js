6dd6ecf82ecff1f616ea999c7ffd1087d78a2576
Parvez Saifi <parvezs2442@gmail.com>
2026-07-18 00:05:04 +0530

    Upload pdf & extracts data from PDF

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from "express"
 import dotenv from "dotenv"
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
 import { ChatGroq } from "@langchain/groq"
+import fs from "fs"
+import { PDFParse } from "pdf-parse"
 
 dotenv.config();
 
@@ -14,6 +16,17 @@ const llm = new ChatGroq({
     model: "openai/gpt-oss-120b",
     temperature: 0,
 })
+
+//Upload the pdf & extract text
+const upload = async() => {
+
+    const pdfPath ="./knowledge.pdf"
+    const buffer =fs.readFileSync(pdfPath)  //converts pdf data into raw binary data
+    const pdfResult = new PDFParse({data:buffer}) //extracts the raw data from pdf
+
+    const textData = await pdfResult.getText() //converts the raw binary data into plain text
+
+}
 
 app.post("/ai", async(req, res) => {
 
